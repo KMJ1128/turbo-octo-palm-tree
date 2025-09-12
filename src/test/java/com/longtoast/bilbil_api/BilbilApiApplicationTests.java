906799743c806bb0aa6772c4347566e4fc91ccfb
KMJ1128 <kim23399551@gmail.com>
2025-09-12 23:34:38 +0900

    새로운 시작

---
+++ src/test/java/com/longtoast/bilbil_api/BilbilApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.longtoast.bilbil_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilbilApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
