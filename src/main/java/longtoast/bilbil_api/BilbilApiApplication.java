f8b51bbfb7c49789f30656c34b3b47ce0f269824
tkqnw <tkqnwk0809@gmail.com>
2025-09-23 20:42:36 +0900

    Commit 테스트

---
+++ src/main/java/longtoast/bilbil_api/BilbilApiApplication.java
@@ -0,0 +1,13 @@
+package longtoast.bilbil_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilbilApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilbilApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/longtoast/bilbil_api/BilbilApiApplicationTests.java
@@ -0,0 +1,13 @@
+package longtoast.bilbil_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilbilApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
